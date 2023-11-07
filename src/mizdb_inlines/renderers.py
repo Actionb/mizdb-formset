b9998004124d773eab45ee08b290015c635b829b
Actionb <yummytea1@gmail.com>
2023-11-07 14:22:19 +0100

    Change inline delete button

--- src/mizdb_inlines/renderers.py
+++ src/mizdb_inlines/renderers.py
@@ -34,7 +34,7 @@ class DeleteFieldRenderer(FieldRenderer):
 
     def get_button_class(self):
         """Return the CSS classes for the delete button."""
-        return "btn w-100 btn-outline-secondary btn-sm inline-delete-btn"
+        return "btn p-0 w-100 text-danger inline-delete-btn"
 
     def get_button_title(self):
         """Return the title attribute for the delete button."""
@@ -42,7 +42,7 @@ class DeleteFieldRenderer(FieldRenderer):
 
     def get_button_content(self):
         """Return the content for the delete button."""
-        return """<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-delete"><path d="M21 4H8l-7 8 7 8h13a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2z"></path><line x1="18" y1="9" x2="12" y2="15"></line><line x1="12" y1="9" x2="18" y2="15"></line></svg>"""  # noqa
+        return """<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>"""  # noqa
 
     def get_button_html(self):
         """Render the HTML for the delete button."""
