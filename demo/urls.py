daf50f76cae3e30fe7b260562e98fd31aaa735a0
Actionb <yummytea1@gmail.com>
2024-06-15 11:14:24 +0200

    Reformat code

--- demo/urls.py
+++ demo/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from app.views import ContactView
 from django.contrib import admin
 from django.urls import path
